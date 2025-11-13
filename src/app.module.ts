e89709337963d97caa30966e5f007f4aa42a8436
FrazKhan1 <fraz9838@gmail.com>
2025-11-13 14:57:01 +0500

    hide exposed db url

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,8 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://fraz:Perplex@cluster0.b7fiv6s.mongodb.net/nest-auth',
-    ),
     ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URL as string),
     AuthModule,
     UserModule,
   ],
